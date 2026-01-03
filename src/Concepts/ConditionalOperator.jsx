6f1119c74f0f808187520399b487b7a858a3e318
Mohammad Shahid <www.shahid6516@gmail.com>
2026-01-03 23:55:31 +0530

    Finish the codtionals and operator lesson

--- src/Concepts/ConditionalOperator.jsx
+++ src/Concepts/ConditionalOperator.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const ConditionalOperator = () => {
-  return (
-    <div>ConditionalOperator</div>
-  )
+    return (
+        <div>
+            <h2>Question 1: Diffrence between && vs ||</h2>
+            & operator is return true if the both condition is true and || is return true if any condition is true
+            <hr />
+            <h2>Question 2: what is the diffrence between .? vs ??</h2>
+
+            .? is use for optional chaining and ?? use for conditional rendiring 
+        </div>
+    )
 }
 
-export default ConditionalOperator
+export default ConditionalOperator 
