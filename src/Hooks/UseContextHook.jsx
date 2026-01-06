4147da0bbe63f5b29d32852ee69a9ef4b123488a
Mohammad Shahid <www.shahid6516@gmail.com>
2026-01-07 01:18:29 +0530

    Solved the question in the useContext

--- src/Hooks/UseContextHook.jsx
+++ src/Hooks/UseContextHook.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const UseContextHook = () => {
-  return (
-    <div>UseContextHook</div>
-  )
+    return (
+        <div>
+            <h1>UseContext Hook</h1>
+            <h2>Question 1: What is useContext in React?</h2>
+            <p>
+                - Used to consume values (like data or functions) from a context created by createContext().
+                - it allows functional components to access context values without prop drilling.
+                - In scenarios where passing props thorugh multiple levels of components is impractical.
+            </p>
+        </div>
+    )
 }
 
 export default UseContextHook
