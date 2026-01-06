d6f116aa95bc049fc9cb6723c8a633e13c06b249
Mohammad Shahid <www.shahid6516@gmail.com>
2026-01-06 11:50:03 +0530

    Solved question one of useEffect

--- src/Hooks/useEffect.jsx
+++ src/Hooks/useEffect.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const useEffect = () => {
-  return (
-    <div>useEffect</div>
-  )
+
+    useEffect(() => {
+        // side effect code goes here
+        return () => {
+            // clean up code (optional)
+            // runs on component unmount or before re-runs
+        }
+
+    }, [
+        // dependencies
+    ])
+
+    return (
+        <div>
+            <h3>Question 1: what is useEffect in React?</h3>
+            <p>useEffect is a hook in functional components to perform side effect after rendering, such as data fetching, subscription or manual dom manipulation.</p>
+        </div>
+    )
 }
 
 export default useEffect
